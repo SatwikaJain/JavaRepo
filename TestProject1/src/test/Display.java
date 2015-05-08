90778883d48b013b8e4091e2f0cf2dcc7c93da6d
SatwikaJain <satwika.jain@infogain.com>
2015-05-08 12:58:48 +0530

    Importing TestProject2

---
+++ TestProject1/src/test/Display.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Satwika.Jain
+ *
+ */
+public class Display {
+
+	static public void displayText()
+	{
+		System.out.println("Display Sample Text");
+	}
+}
---
+++ TestProject2/src/test/git/CallDisplay.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package test.git;
+
+import test.Display;
+
+/**
+ * @author Satwika.Jain
+ *
+ */
+public class CallDisplay {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display.displayText();
+
+	}
+
+}
